cc6bee992064aef6ee9cb86218b9bcef06c4b8c4
Nate Kurt <nate.kurt@gmail.com>
2020-07-30 12:59:14 -0400

    added better logging information

--- convert_browser_to_http.py
+++ convert_browser_to_http.py
@@ -14,6 +14,7 @@ import functools
 import logging
 from itertools import chain
 from datetime import datetime
+from sys import stdout
 
 
 
@@ -23,8 +24,12 @@ class InputError(Exception):
         self.message = message
 
 
-#logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', filemode='w', filename="logs/" + datetime.now().strftime('b2http_logfile_%H_%M_%d_%m_%Y.log'), datefmt='%m-%d-%Y %H:%M:%S', level=logging.DEBUG)
-logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=logging.DEBUG)
+
+
+logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', filemode='a', filename="logs/" + datetime.now().strftime('b2http_logfile_%H_%M_%d_%m_%Y.log'), datefmt='%m-%d-%Y %H:%M:%S', level=logging.DEBUG)
+#logging.basicConfig(datefmt='%m-%d-%Y %H:%M:%S')
+
+#logging.basicConfig(level=logging.INFO, filemode='w')
 
 
 parser = argparse.ArgumentParser(description="Convert Browser Monitors to HTTP Monitors")
@@ -33,11 +38,11 @@ parser.add_argument("url", help="tenant url with SaaS format: https://[tenant_ke
 
 parser.add_argument("token", type=str, help="Your API Token generated with access")
 
-#TODO Maybe Later, but it outputs it to a file anyways so not a huge deal
-#parser.add_argument("-q", "--quiet", help="no output printed to the terminal", action="store_true")
+debug_level_group = parser.add_mutually_exclusive_group()
 
-#TODO
-#parser.add_argument("--debug", help="prints better logging message", action="store_true")
+debug_level_group.add_argument("-q", "--quiet", help="no output printed to the terminal", action="store_true")
+
+debug_level_group.add_argument("--debug", help="prints better logging message", action="store_true")
 
 #Lists the management zones in case you don't know the ID's to hit. 
 parser.add_argument("-l", "--list", help="list the management zone names and IDs, as well as HTTP location names and IDs", action="store_true")
@@ -50,7 +55,7 @@ parser.add_argument("--location", nargs="+", action="append", help="Add location
 parser.add_argument("--overwrite", help="If there is an existing HTTP Monitor with the same name, overwrite it", action="store_true")
 
 #If there are certain tags of monitors that you don't want to transfer over, you can go ahead and set those here in a list
-#TODO
+
 parser.add_argument("--exclude-tag", help="add tags that you want excluded from being transferred over, each tag you want excluded use the arg again", action="append")
 
 parser.add_argument("--include_tag", help="Add tags that you want included to be transferred over as well. If you have a management zone listed,\
@@ -62,20 +67,19 @@ parser.add_argument("-f", "--frequency", help="sets the frequency of the new mon
 
 
 
-
 ## A mutually exclusive group so you can only ask for keep old or delete old. 
 group1 = parser.add_mutually_exclusive_group()
 
-#TODO
+
 group1.add_argument("-k", "--keep_old", help="doesn't disable the old browser monitors so you have two running concurrently", action="store_true")
 
-#TODO
+
 group1.add_argument("--delete", help="Deletes the old browser monitor", action="store_true")
 
-#TODO
+
 group1.add_argument("-d", "--disable", help="Disables old browser monitors but still keeps them there", action="store_true")
 
-#TODO
+
 group1.add_argument("--dry_run", help="Doesn't actually post but does everything else to see if there would be an error", action="store_true")
 
 #Can only select some of these
@@ -96,10 +100,56 @@ mz_group.add_argument("-s", "--select_monitor_id", help="Gets a single browser m
 args = parser.parse_args()
 
 
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+c_handler = logging.StreamHandler(stream=stdout)
+f_name = "logs/" + datetime.now().strftime('b2http_logfile_%H_%M_%m-%d-%Y.log')
+f_handler = logging.FileHandler(f_name, mode='w')
+
+
+
+f_format = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s", datefmt='%m-%d-%Y %H:%M:%S')
+
+c_format = logging.Formatter("[%(levelname)s] %(message)s")
+c_handler.setFormatter(c_format)
+f_handler.setFormatter(f_format)
+
+f_handler.setLevel(logging.DEBUG)
+logger.addHandler(f_handler)
+
+if args.debug:
+    logger.setLevel(logging.DEBUG)
+    c_handler.setLevel(logging.DEBUG)
+    logger.addHandler(c_handler)
+elif not args.quiet:
+    c_handler.setLevel(logging.INFO)
+    logger.addHandler(c_handler)
+
+
+
+# logger.debug("debug message")
+# logger.info("info message")
+# logger.warning("warn message")
+# logger.error("error message")
+# logger.critical("critical message")
+
+#Santizie Endpoint
+#Change http to https if there
+args.url = args.url.replace("http://", "https://")
+# logger.debug("HTTP in url detected, using https instead")
+try:
+    args.url = args.url.rstrip("/ ")
+except: 
+    logger.error(f"URL: {args.url} Malformed")
+
+
+
 # tenant = args.url
 
-# if args.mz
-# management_zone = args.m
+#frequency list that contains possible values for frequencies you can have. 
 frequency_ls = [5,10,15,30,60,120,180]
 
 api_token = args.token
@@ -109,10 +159,11 @@ if args.frequency:
 
     try:
         val = int(args.frequency)
-        assert val in frequency_ls, logging.critical(f"Assertion Error: Frequency must be either {','.join(map(str, frequency_ls))}")
+        assert val in frequency_ls, logger.critical(f"Assertion Error: Frequency must be either {','.join(map(str, frequency_ls))}")
 
     except TypeError:
-        logging.error("Frequency Input must be an integer in minutes.")
+        logger.error("Frequency Input must be an integer in minutes.")
+
     
 
 
@@ -161,7 +212,7 @@ class Request(object):
             if payload is not None:
               args['json'] = payload
 
-            logging.debug(f"Request URL: {args['url']}")
+            logger.debug(f"{self.method} Request URL: {args['url']}")
               
             return requests.request(**args)
         return wrapper
@@ -245,28 +296,34 @@ def dict_synthetic_name_id(func):
         return {element['name']: element['entityId'] for element in func(*args, **kwargs).json()['monitors']}
     return wrapper_list_dict
         
-
+#The class you call for making a certain request- uses the request wrapper
 class MakeRequest(object):
 
     def __init__(self,tenant, r_id="", *args):
         self.tenant = tenant
         self.r_id = r_id
 
+
+    #Get request for all browser monitors
     @GetRequest(endpoint="api/v1/synthetic/monitors?type=BROWSER", args=args)
     def get_browser_monitors(self):
         pass
 
+    #Get All of the browser_monitor Ids
     @list_synthetic_ids
     def get_browser_monitors_ids(self):
         return self.get_browser_monitors()
 
+    #Get management zones names and IDs
     @GetRequest(endpoint="api/config/v1/managementZones")
     def get_management_zones(self):
         pass
         
+    #get just list of management zones IDS
     @list_ids
     def get_management_zones_ids(self):
         return self.get_management_zones()
+
 
     @GetRequest(endpoint="api/config/v1/maintenanceWindows")
     def get_maintenence_windows(self):
@@ -319,12 +376,6 @@ class MaintenenceWindow:
     def get_window(self):
         pass
 
-    def get_new_monitors(self):
-        pass
-
-    def update_windows(self):
-        pass
-        
     def __str__(self):
         return self.get_window().text
 
@@ -353,11 +404,6 @@ class SyntheticMonitor:
         tags_json = [element['key'] for element in self.b_json['tags']]
         return tags_json
 
-    def __str__(self):
-        return self.get_monitor().text
-
-    def __repr__(self):
-        return pformat(self.get_monitor().json())
 
     @DeleteRequest(endpoint="/api/v1/synthetic/monitors/", r_id='self.r_id')
     def delete_monitor(self):
@@ -367,6 +413,13 @@ class SyntheticMonitor:
     def disable_monitor(self):
         self.b_json["enabled"] = False
         return self.b_json
+
+    
+    def __str__(self):
+        return self.get_monitor().text
+
+    def __repr__(self):
+        return pformat(self.get_monitor().json())
     
 
 class HttpMonitor(SyntheticMonitor):
@@ -444,11 +497,9 @@ class BrowserMonitor(SyntheticMonitor):
   "automaticallyAssignedApps": []
 }'''
 
+
         json_dict = json.loads(json_data)
-
         
-        
-
         
         json_dict["name"] = self.b_json["name"] + " Now HTTP"
         #transfers tags over
@@ -548,7 +599,7 @@ def get_eligible_locations(http_locations, browser_locations,b_id):
         if location in [http["entityId"] for http in http_locations]:
             eligible_locations.append(location)
         else:
-            logging.warn(f"Browser Monitor {b_id} using location {location} which either isn't supported yet for HTTP Monitors or Does not exist")
+            logger.warn(f"Browser Monitor {b_id} using location {location} which either isn't supported yet for HTTP Monitors or Does not exist")
     
     return eligible_locations
 
@@ -594,7 +645,7 @@ else:
 
     elif not args.all:
         #Raise Exception and say you haven't listed anything.
-        logging.critical("Input Error: Must Select Filter for monitors, if you want all monitors, make sure to use -a")
+        logger.critical("Input Error: Must Select Filter for monitors, if you want all monitors, make sure to use -a")
         raise InputError("Input Error", "Must select filter for monitors, if all make sure to use -a")
     else:
         browser_monitor_ids = api.get_browser_monitors_ids()
@@ -625,7 +676,7 @@ else:
 
 
 
-    assert browser_monitor_ids != [], logging.critical("Assertion Error, No Monitors in this scope")
+    assert browser_monitor_ids != [], logger.critical("Assertion Error, No Monitors in this scope")
 
     
     for b_id in browser_monitor_ids:
@@ -634,7 +685,7 @@ else:
         b_monitor_type = monitor_obj.b_json["script"]["type"]
         #A browser clickpath monitor which isn't supported
         if b_monitor_type != "availability":
-            logging.warn(f"Browser Clickpath ID: {b_id} Not supported, skipping")
+            logger.warn(f"Browser Clickpath ID: {b_id} Not supported, skipping")
             continue
 
         #If they wanted excluded tags, checks if they used the args and it worked. 
@@ -650,12 +701,12 @@ else:
         if not args.location:
             loc_list = get_eligible_locations(http_locations, monitor_obj.b_json["locations"],b_id)
             if not loc_list:
-                logging.critical(f"Browser Monitor {monitor_obj.b_json['name']}(ID: {b_id}) Doesn't have any locations set that are available for public http monitors, you can set a location in the arguments")
+                logger.critical(f"Browser Monitor {monitor_obj.b_json['name']}(ID: {b_id}) Doesn't have any locations set that are available for public http monitors, you can set a location in the arguments")
                 continue
         else:
             loc_list = get_eligible_locations(http_locations, args.location, b_id)
             if not loc_list:
-                logging.critical(f"Monitor Locations You Chose are either ineligible or don't exist, check ids that do exist by using the --list command")
+                logger.critical(f"Monitor Locations You Chose are either ineligible or don't exist, check ids that do exist by using the --list command")
                 continue
         
 
@@ -665,7 +716,7 @@ else:
             #check if authentication in event, if it is, make sure it's not webform
             if "authentication" in event:
                 if event["authentication"]["type"] == "webform":
-                    logging.warn("Authentication Type is Webform, Skipping Monitor")
+                    logger.warn("Authentication Type is Webform, Skipping Monitor")
                     break
 
     
@@ -687,7 +738,8 @@ else:
 
             response = monitor_obj.create_http()
             if response.status_code >= 400:
-                logging.error(f"can't post monitor Name: {monitor_obj.b_json['name']}, ID: {b_id}")
+                
+                logger.error(f"can't post monitor Name: {monitor_obj.b_json['name']}, ID: {b_id}")
                 failed_monitors.update({monitor_obj.b_json['name']:b_id})
                 continue
             
@@ -698,34 +750,34 @@ else:
                 del_response = del_api.delete_monitor()
                 #pprint(f"Deletion Status Code: {del_response.status_code}")
                 #maybe change the assertion stuff eventually
-                assert del_response.status_code < 400, logging.error(f"Unable to delete monitor {del_id}\
+                assert del_response.status_code < 400, logger.error(f"Unable to delete monitor {del_id}\
                     Error Code: {del_response.status_code}")
-                logging.info(f"Overwrote HTTP Monitor: {del_id}")
+                logger.info(f"Overwrote HTTP Monitor: {del_id}")
 
 
             
             if args.delete and response.status_code < 400:
                 del_b_monitor_response = monitor_obj.delete_monitor()
-                assert del_b_monitor_response.status_code < 400, logging.error(f"Unable to delete browser monitor {b_id}\
+                assert del_b_monitor_response.status_code < 400, logger.error(f"Unable to delete browser monitor {b_id}\
                     Error Code: {del_b_monitor_response.status_code}")
-                logging.info(f"Deleted Old Browser Monitor, Name: {monitor_obj.b_json['name']} ID: {b_id}")
+                logger.info(f"Deleted Old Browser Monitor, Name: {monitor_obj.b_json['name']} ID: {b_id}")
             elif args.disable and response.status_code < 400:
                 disable_b_monitor_response = monitor_obj.disable_monitor()
-                assert disable_b_monitor_response.status_code < 400, logging.error(f"Unable to disable browser monitor {b_id} \
+                assert disable_b_monitor_response.status_code < 400, logger.error(f"Unable to disable browser monitor {b_id} \
                     Error Code: {disable_b_monitor_response.status_code}")
 
             
             assert(response.status_code < 400)
             #pprint(response.json())
             http_id = response.json()["entityId"]
-            logging.debug("HTTP ID: " + http_id)
+            logger.debug("HTTP ID: " + http_id)
             b_monitor_http_monitor_dict.update({b_id:http_id})
 
-            logging.info(f"Creating Monitor from Old Browser Monitor ID: {b_id} New Browser Monitor ID: {http_id}")
+            logger.info(f"Creating Monitor from Old Browser Monitor ID: {b_id} New Browser Monitor ID: {http_id}")
 
         #empty list so no requirements met. 
 
-        assert b_monitor_http_monitor_dict.keys(), logging.critical("Error: No Monitors in this scope")
+        assert b_monitor_http_monitor_dict.keys(), logger.critical("Error: No Monitors in this scope")
 
         
         
@@ -738,7 +790,7 @@ else:
                 #if it's an all env one, or tag one don't need to bother because it will already be transferred over
                 if m_id in b_monitor_http_monitor_dict.keys():
                     m_window_obj.window_json["scope"]["entities"].append(b_monitor_http_monitor_dict[m_id])
-                    logging.info(f"Adding HTTP Monitor {b_monitor_http_monitor_dict[m_id]} to Maintenence Window {item}")
+                    logger.info(f"Adding HTTP Monitor {b_monitor_http_monitor_dict[m_id]} to Maintenence Window {item}")
                     #Put Request for Window
                     #pprint(m_window_obj.window_json)
 
@@ -746,7 +798,7 @@ else:
         pprint("List of failed monitors: ")
         print('\n'.join(['Name: {0:<15} Id: {1}'.format(k,v) for k,v in failed_monitors.items()]))
     else:
-        logging.info("All Monitors Successfully Created")
+        logger.info("All Monitors Successfully Created")
 
 
 
